ac4f02ac197b8150110711bbac0602d96ce48665
omid <omidjafari3031@gmail.com>
2022-02-16 19:14:10 +0330

    body1 compilted

--- components/Body1.js
+++ components/Body1.js
@@ -3,14 +3,16 @@ import styles from '../styles/Body1.module.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BiSearch } from 'react-icons/bi'
 import { IoMdArrowDropdown } from 'react-icons/io'
+import { BsFillCalculatorFill } from 'react-icons/bs'
+import { MdLocationOn } from 'react-icons/md'
 
 
 
 const Body1 = () => {
     return (
         <div className={styles.wrapper}>
-            <div className='container align-items-center p-4'>
-                <div className="d-flex flex-row justify-content-between bg-white mt-5 align-items-center">
+            <div className='container align-items-center '>
+                <div className="d-flex flex-row justify-content-between bg-white mt-5 align-items-center ms-2">
                     <div className="p-2 ps-4">items&nbsp;<IoMdArrowDropdown /></div>
                     <div className="p-2 ps-4 border-start border-1 border-secondary">items&nbsp;<IoMdArrowDropdown /></div>
                     <div className="p-2 ps-4 border-start border-1 border-secondary">items&nbsp;<IoMdArrowDropdown /></div>
@@ -19,19 +21,16 @@ const Body1 = () => {
                 </div>
             </div>
             <div className='container'>
-                <div className='row'>
-                    <div className='col'>
-                        <div className="row">
-                            <div className="col bg-warning m-2 p-2">Column</div>
-                            <div className="col bg-warning m-2 p-2">Column</div>
-                            <div className="w-100"></div>
-                            <div className="col bg-warning m-2 p-2">Column</div>
-                            <div className="col bg-warning m-2 p-2">Column</div>
-                        </div>
+                <div className='d-flex flex-row py-4'>
+                    <div className={`${styles.info} d-flex flex-wrap`}>
+                        <div className='m-2 p-2'><b><BsFillCalculatorFill />&nbsp;&nbsp;PAYMENT CALCULATOR</b></div>
+                        <div className='m-2 p-2'><b><MdLocationOn size={23}/>&nbsp;&nbsp;GET DIRECTIONS</b></div>
+                        <div className='m-2 p-2'><b>column</b></div>
+                        <div className='m-2 p-2'><b>column</b></div>
                     </div>
-                    <div className='col text-center'>
-                        <div className='text-white text-nowrap'>Tomorrows Hours&nbsp; &nbsp; Sales:10:00AM-7:00pm&nbsp; &nbsp; Service & Parts: 11:00am-5:00pm</div>
-                        <div className={styles.button}>VIEW HOURS</div>
+                    <div className={`${styles.hours}`}>
+                        <p className='text-nowrap py-2' style={{fontSize:14}}>Tommorows Hours: &nbsp; &nbsp;Sales:10:00AM-7:00PM &nbsp; &nbsp;Service and Parts: 11:00AM-5:00PM</p>
+                        <div className={styles.viewButton}>VIEW HOURS</div>
                     </div>
                 </div>
             </div>
