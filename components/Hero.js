b2137766e70ff9ae026b36b5792b0e50ae3542d0
omid <omidjafari3031@gmail.com>
2022-02-16 11:00:39 +0330

    compilited navbar

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Hero.module.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Hero = () => {
+  return (
+    <div className={styles.wrapper}>
+      <img className='img-fluid' src='/lambo.png' alt='lambo' />
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/Nav.js
@@ -0,0 +1,56 @@
+import styles from '../styles/Nav.module.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'next/image';
+import { FaTelegramPlane } from 'react-icons/fa'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { MdLocationOn } from 'react-icons/md'
+import { BsFillTelephoneFill } from 'react-icons/bs'
+import { IoMdArrowDropdown } from 'react-icons/io'
+
+
+const Nav = () => {
+  return (
+    <div className='container-xl'>
+      <div className={styles.nav}>
+
+        <div className={styles.contact}>
+          <ul>
+            <li ><Image src='/logo.png' width={143} height={59} /></li>
+            <li>
+              <div>
+                <span>
+                  <BsFillTelephoneFill />
+                  (604) 500-3131
+                </span> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <span>
+                  <MdLocationOn />
+                  12826King George Hwy Surrey Biritish Columbia V3T 259
+                </span>&nbsp; &nbsp; &nbsp; &nbsp;
+                <span>
+                  <AiOutlineTwitter />
+                </span>&nbsp; &nbsp;
+                <span>
+                  <FaTelegramPlane />
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.navigate}>
+         <div className='container'>
+         <div className="row">
+            <div className="col text-center pt-1 border-right border-white">items</div>
+            <div className="col text-center pt-1 border-right border-white">items&nbsp;<IoMdArrowDropdown /></div>
+            <div className="col text-center pt-1 border-right border-white">items&nbsp;<IoMdArrowDropdown /></div>
+            <div className="col text-center pt-1 border-right border-white">items&nbsp;<IoMdArrowDropdown /></div>
+            <div className="col text-center pt-1 border-right border-white">items&nbsp;<IoMdArrowDropdown /></div>
+            <div className="col text-center pt-1 border-right border-white">items</div>
+          </div>
+         </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
